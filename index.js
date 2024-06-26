a4dee04f5332b7b8ba5c90031a522939f09df2a5
H3ristos <estebanpicherit@gmail.com>
2024-06-26 13:15:28 +0200

    m

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow () {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        icon: path.join(__dirname, 'assets', 'icon.ico'), // Spécifiez le chemin vers votre icône
         webPreferences: {
             nodeIntegration: true
         }
