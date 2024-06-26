33b7ca66877ac7ec16ec3341997ac1c9788ceb1f
H3ristos <estebanpicherit@gmail.com>
2024-06-26 13:42:05 +0200

    m

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 
-function createWindow () {
-    const win = new BrowserWindow({
-        width: 1256,
-        height: 700,
-        icon: path.join(__dirname, 'assets', 'icon.ico'), // Spécifiez le chemin vers votre icône
-        webPreferences: {
-            nodeIntegration: true
-        }
-    });
+function createLoadingWindow() {
+   const loadingWin = new BrowserWindow({
+       width: 300,
+       height: 300,
+       frame: false,
+       transparent: true,
+       alwaysOnTop: true,
+       icon: path.join(__dirname, 'assets', 'icon.ico'),
+       webPreferences: {
+           nodeIntegration: true
+       }
+   });
 
-    win.loadFile('index.html');
+   loadingWin.loadFile('loading.html');
+
+   return loadingWin;
 }
 
-app.on('ready', createWindow);
+function createMainWindow() {
+   const mainWindow = new BrowserWindow({
+       width: 1256,
+       height: 700,
+       minWidth: 1256,
+       minHeight: 700,
+       icon: path.join(__dirname, 'assets', 'icon.ico'),
+       webPreferences: {
+           nodeIntegration: true
+       },
+       show: false
+   });
+
+   mainWindow.loadFile('index.html');
+
+   mainWindow.once('ready-to-show', () => {
+       setTimeout(() => {
+           mainWindow.show();
+           if (loadingWindow) {
+               loadingWindow.close();
+           }
+       }, 3000);
+   });
+
+   return mainWindow;
+}
+
+let loadingWindow;
+app.on('ready', () => {
+   loadingWindow = createLoadingWindow();
+   const mainWindow = createMainWindow();
+});
